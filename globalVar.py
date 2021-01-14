1f4e4d23dd83025390fa1118f708e6cdde9f1068
jiangjilian <jiangjilian@mail.iggcas.ac.cn>
2021-01-14 14:02:22 +0800

    2021.1.13

--- globalVar.py
+++ globalVar.py
@@ -2,10 +2,10 @@
 
 global Path, outputPath, dataPath, dataPath1, dataPath2, figPath, elements
 test_count = str("CLR_all_element")
-Path = ".\\"
-outputPath = ".\\result\\"
-dataPath = ".\\data\\"
-figPath = ".\\fig\\"
+Path = "./"
+outputPath = "./result/"
+dataPath = "./data/"
+figPath = "./fig/"
 
 elements = ['P', 'Y', 'Ce', 'Sm', 'Eu', 'Dy', 'Lu', 'Hf', 'Th', 'U']
 # info_list = ['No', 'Set', 'zircon', 'Sample ID', 'Rock type', 'Age(Ma)', "REE+Y", "label"]
